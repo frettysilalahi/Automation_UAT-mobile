b36cb4fda76872fe82556a401814dcfdae3a90d9
frettysilalahi <fretty.s@tokenomy.com>
2023-07-05 07:45:45 +0700

    05-07-2023

--- Scripts/TC_Dashboard_Asset/Script1687841885670.groovy
+++ Scripts/TC_Dashboard_Asset/Script1687841885670.groovy
@@ -17,7 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-Mobile.startApplication('D:\\11419057_Fretty L M Silalahi\\Tokenomy\\2.9.0-UAT.apk', true)
+Mobile.startApplication('D:\\11419057_Fretty L M Silalahi\\Tokenomy\\2.10.0-UAT(300641).apk', true)
 
 Mobile.tap(findTestObject('Object Repository/Object_Login/android.widget.TextView - SKIP'), 0)
 
